6416f318c73f050c3f713b9015eca24e7be0ece7
Paul <pwittschen@gmail.com>
2019-08-30 06:50:20 -0400

    added bar and pie charts

--- src/app/charts/bar-chart/bar-chart.component.ts
+++ src/app/charts/bar-chart/bar-chart.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
+const data: any[] = [
+  { data: [1, 2, 3, 4, 5, 6, 7], label: 'my label 1' },
+  { data: [11, 22, 33, 14, 35, 26, 17], label: 'my label 2' }
+];
+
+const labels: string[] = [ 'W1', 'W2', 'W3', 'W4', 'W5', 'W6', 'W7'];
+
 @Component({
   selector: 'app-bar-chart',
   templateUrl: './bar-chart.component.html',
@@ -8,6 +15,15 @@ import { Component, OnInit } from '@angular/core';
 export class BarChartComponent implements OnInit {
 
   constructor() { }
+
+  public barChartData: any[] = data;
+  public barChartLabels: string[] = labels;
+  public barChartType: string = 'bar';
+  public barChartLegend: boolean = false;
+  public barChartOptions: any = {
+    scaleShowVerticalLines: false,
+    responsive: true
+  }
 
   ngOnInit() {
   }
--- src/app/charts/pie-chart/pie-chart.component.ts
+++ src/app/charts/pie-chart/pie-chart.component.ts
@@ -9,6 +9,16 @@ export class PieChartComponent implements OnInit {
 
   constructor() { }
 
+  public pieChartData: number[] = [350, 450, 120];
+  public pieChartLabels: string[] = ['Jim', 'Michael', 'Dwight'];
+  public colors: any[] = [
+    { 
+      backgroundColor: [ '#26547c', '#ff6b6b', '#ffd166' ],
+      // borderColor: #fff
+    }
+  ];
+  public pieChartType: string = 'pie';
+
   ngOnInit() {
   }
 
